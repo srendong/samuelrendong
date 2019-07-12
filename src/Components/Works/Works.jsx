f1c37f022079ac1614039944d00a7246589b66b6
Samuel Rendon G <s.redon.g@gmail.com>
2019-07-12 11:43:30 -0500

    final responsive all devices

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const Works = () => {
     return(
         <div className="works container-fluid justify-content-center ">
-            <div className="contenido border">
+            <div className="contenido ">
                     <h3 className="">Personal works...</h3>
-                <div className="proyects border">
+                <div className="proyects ">
                     <div className="proyect"></div>
                     <div className="proyect"></div>
                     <div className="proyect"></div>
